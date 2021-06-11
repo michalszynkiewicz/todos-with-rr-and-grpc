992d7798e281cf8d7c148959ea2aefeac261481e
Michał Szynkiewicz <michal.l.szynkiewicz@gmail.com>
2021-06-11 10:45:23 +0200

    modified storage, resource failing to compile

--- src/main/java/com/example/storage/TodoStorage.java
+++ src/main/java/com/example/storage/TodoStorage.java
@@ -19,8 +19,8 @@ public class TodoStorage {
         );
     }
 
-    public Stream<Todo> streamAll() {
-        return null; //Todo.streamAll();
+    public Multi<Todo> streamAll() {
+        return Todo.streamAll();
     }
 
     @Transactional
